eae03e18dd2dad9d41a2b17083363948d59cb961
DenQ <denquick@gmail.com>
2015-08-07 18:32:15 +0300

    Include bootstrap

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-menu'
+  name: 'ember-menu',
+  included: function(app) {
+    this._super.included(app);
+
+    app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+    app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+  }
 };
